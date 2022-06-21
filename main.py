b8137d020712ef2ef5495143df9834b160c4ffdc
MH Augustine Kim <roives.kim21@gmail.com>
2022-06-21 12:59:00 +0900

    브랜치 이름변경

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+import psycopg2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
